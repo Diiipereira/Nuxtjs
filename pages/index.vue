156a4982f769ce9ccf6123ccfaeba6921120268a
Diogo Pereira <dipereira@live.com>
2021-10-22 01:11:32 -0300

    A simple project to get to know the structure os Nuxtjs

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <ColorModePicker />
+  </div>
+</template>
+
+<script>
+import ColorModePicker from '@/components/ColorModePicker'
+export default {
+  components: {
+    ColorModePicker
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    text-align: center;
+    padding: 50px;
+  }
+</style>
